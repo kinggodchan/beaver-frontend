e6cf14c3806a6bb6359c79d42300b9ff2a154e8b
kinggodchan <kyc010264@gmail.com>
2025-03-17 18:33:56 +0900

    feat: 정보 게시판

--- src/boards/InformationBoard.js
+++ src/boards/InformationBoard.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-function InformationBoard() {
-  return <h2>정보 게시판 페이지</h2>;
-}
+const InformationBoard = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/posts')
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(error => {
+        console.error('게시글 불러오기 오류:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>정보 게시판</h1>
+      <div className="post-list">
+        {posts.map(post => (
+          <div key={post.id} className="post-item">
+            <Link to={`/post/${post.id}`}>
+              <img src={post.imageUrl} alt={post.title} />
+              <h2>{post.title}</h2>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default InformationBoard;
