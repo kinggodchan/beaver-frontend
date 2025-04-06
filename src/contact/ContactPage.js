8218379543eecde326629fc2664bb33a2798fcbb
kinggodchan <kyc010264@gmail.com>
2025-04-06 19:07:56 +0900

    fix: 문의하기 페이지 수정

--- src/contact/ContactPage.js
+++ src/contact/ContactPage.js
@@ -1,20 +1,18 @@
 import React from "react";
-import { useNavigate } from "react-router-dom"; // ✅ Link와 useNavigate 추가
+import { useNavigate } from "react-router-dom"; 
 import "./ContactPage.css";
 import Header from "../component/Header";
 
 const ContactPage = () => {
-  const navigate = useNavigate(); // ✅ 페이지 이동을 위한 useNavigate 사용
+  const navigate = useNavigate(); 
 
   return (
     <>
-      <Header /> {/* ✅ 헤더 컴포넌트 추가 */}
+      <Header /> 
       <div className="contact-container">
         <div className="contact-content">
-          {/* ✅ 이미지 (public 폴더 접근) */}
           <img src="/question.png" alt="Question" className="question-image" />
 
-          {/* ✅ 폼 영역 */}
           <form className="contact-form">
             <div className="form-group">
               <label>Name</label>
@@ -36,13 +34,11 @@ const ContactPage = () => {
               <textarea placeholder="Enter your message"></textarea>
             </div>
 
-            {/* ✅ 버튼 그룹 */}
             <div className="button-group">
               <button type="submit">Submit</button>
-              <button type="button" onClick={() => navigate("/menu")}>
+              <button type="button" onClick={() => navigate("/inquiry")}>
                 List
               </button>{" "}
-              {/* ✅ 버튼 클릭 시 /menu 이동 */}
             </div>
           </form>
         </div>
